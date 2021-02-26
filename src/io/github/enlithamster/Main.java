9c36a1def46da6f6bee516627989c15d970f2ea5
Sandro Massa <san.massa.98@gmail.com>
2021-02-26 15:10:47 +0100

    Cleanup

--- src/io/github/enlithamster/Main.java
+++ src/io/github/enlithamster/Main.java
@@ -6,28 +6,28 @@ import java.util.Scanner;
 import io.github.enlithamster.one.DayOne;
 
 public class Main {
-    
-    public static void main(String[] args) {
 
-        // Menu printing
-        System.out.print(
-            "by Sandro Massa (aka EnlitHamster, EnlitOwl)\n"
-        +   "\n"
-        +   "[1] Report Repair\n"
-        +   "\n"
-        +   "> "
-        );
+  public static void main(String[] args) {
 
-        // Input catching
-        Scanner input = new Scanner(System.in);
-        int selection = input.nextInt();
-        input.close();
+    // Menu printing
+    System.out.print(
+    "by Sandro Massa (aka EnlitHamster, EnlitOwl)\n"
+    +   "\n"
+    +   "[1] Report Repair\n"
+    +   "\n"
+    +   "> "
+    );
 
-        // Execution of correct day code
-        switch (selection) {
-            case 1: (new DayOne()).launch();
-        }
+    // Input catching
+    Scanner input = new Scanner(System.in);
+    int selection = input.nextInt();
+    input.close();
 
+    // Execution of correct day code
+    switch (selection) {
+      case 1: (new DayOne()).launch();
     }
+
+  }
 
 }
--- src/io/github/enlithamster/one/DayOne.java
+++ src/io/github/enlithamster/one/DayOne.java
@@ -1,66 +1,95 @@
 package io.github.enlithamster.one;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
+import java.io.InputStream;
+
 import io.github.enlithamster.DayChallenge;
-import io.github.enlithamster.FileLoader;
 
 public class DayOne implements DayChallenge {
 
-    public void launch() {
+  public void launch() {
 
-        // Setup
-        ArrayList<Integer> numbers = new ArrayList<>();
-        
-        try {
-            // Reading numbers from input file
-            File input = FileLoader.getFile2("one/input.txt");
-            Scanner fileReader = new Scanner(input);
-            while (fileReader.hasNextInt()) numbers.add(fileReader.nextInt());
-            fileReader.close();
+    // Setup
+    ArrayList<Integer> numbers = new ArrayList<>();
 
-            // Sorting the numbers
-            Collections.sort(numbers);
-            Integer[] keys = getKeys(numbers, 2020);
-            if (keys != null) System.out.println("The searched numbers are " + keys[0] + " and " + keys[1] + ".");
-            else System.out.println("No keys found.");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+    try {
+      // Reading numbers from input file
+      InputStream input = getClass().getResourceAsStream("/resources/one/input.txt");
+      Scanner fileReader = new Scanner(input);
+      while (fileReader.hasNextInt()) numbers.add(fileReader.nextInt());
+      fileReader.close();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
 
-    /**
-     * Binary search algorithm for the two keys.
-     * 
-     * @param numbers List of sorted numbers to research
-     * @param target Target sum to find
-     * @return
-     */
-    private Integer[] getKeys(List<Integer> numbers, Integer target) {
-        // Unsuccessful search
-        if (numbers.isEmpty()) return null;
+    // -- PART 1
 
-        Integer key1 = numbers.get(0);
-        Integer i2 = getKeys(target, key1, numbers);
-        if (i2 != -1) return new Integer[] {key1, numbers.get(i2)};
-        else return getKeys(numbers.subList(1, numbers.size()), target);
-    }
+    // Sorting the numbers
+    Collections.sort(numbers);
 
-    private Integer getKeys(Integer target, Integer key1, List<Integer> numbers) {
-        // Unsuccessful search
-        if (numbers.isEmpty()) return -1;
+    // Searching for two keys
+    Integer[] keys = getBinaryKeys(numbers, 2020);
+    if (keys != null) System.out.println("The searched numbers are " + keys[0] + " and " + keys[1] + ".\nThe answer is " + (keys[0] * keys[1]));
+    else System.out.println("No keys found.");
 
-        Integer pivot = numbers.size() / 2;
-        Integer key2 = numbers.get(pivot);
-        Integer attempt = key1 + key2;
-        if (attempt == target) return pivot;
-        else if (attempt < target) return getKeys(target, key1, numbers.subList(0, pivot));
-        else return getKeys(target, key1, numbers.subList(pivot + 1, numbers.size()));
-    }
+    // -- PART 2
+
+    // Searching for three keys
+    keys = getTernaryKeys(numbers, 2020);
+    if (keys != null) System.out.println("The searched numbers are " + keys[0] + ", " + keys[1] + " and " + keys[2] + ".\nThe answer is " + (keys[0] * keys[1] * keys[2]));
+    else System.out.println("No keys found.");
+
+  }
+
+  /**
+  * Binary search algorithm for the two keys.
+  *
+  * @param numbers List of sorted numbers to search
+  * @param target Target sum to find
+  * @return An array with the two keys, null if not found
+  */
+  private Integer[] getBinaryKeys(List<Integer> numbers, Integer target) {
+    // Unsuccessful search
+    if (numbers.isEmpty()) return null;
+
+    Integer key1 = numbers.get(0);
+    Integer key2 = getBinaryKeys(target, key1, numbers);
+    if (key2 != null) return new Integer[] {key1, key2};
+    else return getBinaryKeys(numbers.subList(1, numbers.size()), target);
+  }
+
+  private Integer getBinaryKeys(Integer target, Integer key1, List<Integer> numbers) {
+    // Unsuccessful search
+    if (numbers.isEmpty()) return null;
+
+    Integer pivot = numbers.size() / 2;
+    Integer key2 = numbers.get(pivot);
+    Integer attempt = key1 + key2;
+    if (attempt.equals(target)) return key2;
+    else if (attempt.compareTo(target) > 0) return getBinaryKeys(target, key1, numbers.subList(0, pivot));
+    else return getBinaryKeys(target, key1, numbers.subList(pivot + 1, numbers.size()));
+  }
+
+  /**
+  * Binary search algorithm for the three keys.
+  *
+  * @param numbers List of sorted numbers to search
+  * @param target Target sum to find
+  * @return An array with the three keys, null if not found
+  */
+  private Integer[] getTernaryKeys(List<Integer> numbers, Integer target) {
+    // Unsuccessful search
+    if (numbers.isEmpty()) return null;
+
+    Integer key1 = numbers.get(0);
+    Integer binaryTarget = target - key1;
+    Integer[] keys = getBinaryKeys(numbers.subList(1, numbers.size()), binaryTarget);
+    if (keys != null) return new Integer[] {key1, keys[0], keys[1]};
+    else return getTernaryKeys(numbers.subList(1, numbers.size()), target);
+  }
 
 }
